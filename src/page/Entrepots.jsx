857b748eb3c67bf6d712c0956da0268944005c8c
Chloé <chloe.du.16@live.fr>
2025-08-01 00:34:01 +0200

    test for fix fetch

--- src/page/Entrepots.jsx
+++ src/page/Entrepots.jsx
@@ -17,12 +17,6 @@ const Entrepots = () => {
 
     const entrepotsAvailables = Object.keys(data);
 
-    useEffect(() => {
-      fetch("https://flask-render-production.up.railway.app/api/depots")
-      .then((res) => res.json())
-      .then((data) => setDepots(data))
-      .catch((err) => console.error("Erreur lors du fetch des dépôts", err))
-    })
   return (
    <>
       <div className="title">
@@ -75,28 +69,6 @@ const Entrepots = () => {
       ) : (
         <p className="placeholder">Sélectionnez un entrepôt pour voir son contenu.</p>
       )}
-      
-      <div className="tableau-depots">
-        <h2> Dépots de Tomates et Emmental</h2>
-        <table>
-          <thead>
-            <tr>
-              <th> Employé</th>
-              <th>Produit</th>
-              <th>Quantité</th>
-            </tr>
-          </thead>
-          <tbody>
-          {depots.map((depot, index) => (
-            <tr key={index}>
-              <td>{depot.joueur}</td>
-              <td>{depot.item}</td>
-              <td>{depot.quantite}</td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      </div>
     </>
   );
 };
